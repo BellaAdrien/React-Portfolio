0367a7fd496104c6771d6993ccc062a76efd0d32
BellaAdrien <Iadrien2020@fau.edu>
2023-11-07 16:47:57 -0500

    save20

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,31 @@
+export default function About() {
+  return (
+    <section id="about">
+              <section className="section-container">
+
+        <h1 className="title">About Me</h1>
+
+
+            <p>My name is Isabella Adrien. I have a Bachelor's degree and now I am working on my fullstack
+                development coding certificate. Below I have several of my projects linked. I look foward to hitting the
+                ground running and gaining experience to help set me apart in the coding world. My contact information
+                is attatched and i am always open to any questions or opportunities. </p>
+
+        </section>
+
+
+    </section>
+
+  );
+}
+
+
+{/* <div>
+      <h1>About me </h1>
+      <p>
+      My name is Isabella Adrien. I have a Bachelor's degree and now I am working on my fulstack
+                development coding certificate. Below I have several of my projects linked. I look foward to hitting the
+                ground running and gaining experience to help set me apart in the coding world. My contact information
+                is attatched and i am always open to any questions or opportunities. 
+      </p>
+    </div> */}
